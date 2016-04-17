6ee5b600f95113190d5a811720186db1305228ff
Evan Krambuhl <thekrambuhl@gmail.com>
2016-04-16 23:30:06 -0700

    remove block

--- slushfile.js
+++ slushfile.js
@@ -27,34 +27,3 @@ gulp.task('modify-package', function(done) {
 gulp.task('default', function(done) {
   sequence(['copy-template', 'modify-package'], done);
 });
-
-gulp.task('default', function (done) {
-  inquirer.prompt([{
-    type: 'input',
-    name: 'name',
-    message: "Give your app a name",
-    // Get app name from arguments by default 
-    default: gulp.args.join(' '),
-  }],
-  function (answers) {
-    if (!answers.moveon) {
-      return done();
-    }
-
-    gulp.src(__dirname + '/template/**', { dot: true }) 
-      .pipe(template(answers))
-      // Rename dotfiles
-      .pipe(rename(function (file) {
-        if (file.basename[0] === '_') {
-          file.basename = '.' + file.basename.slice(1)
-        }
-      }))
-      // Confirms overwrites on file conflicts 
-      .pipe(conflict('./')) 
-      .pipe(gulp.dest('./')) 
-      .pipe(install()) 
-      .on('finish', function () {
-        done();
-      });
-  });
-});
